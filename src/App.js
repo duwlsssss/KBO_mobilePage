55a251d05b4241fb5781614ca331af902199a790
soohyun Kim <rlatngusrla07@naver.com>
2024-03-24 16:52:23 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [userEmail, setUserEmail] = useState('');
+
+  useEffect(() => {
+    // 현재 URL에서 쿼리 파라미터를 추출합니다.
+    const searchParams = new URLSearchParams(window.location.search);
+    // 'userEmail' 파라미터 값을 가져옵니다.
+    const email = searchParams.get('userEmail');
+    // userEmail 상태를 업데이트합니다.
+    if (email) setUserEmail(email);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {userEmail && (
+          <div>
+            {userEmail}에 해당하는 카드 출력 부분
+            console.log({userEmail});
+          </div>
+        )}
+      </div>
+
+      <button>저장하기(이미지)</button>
+      <button>공유하기(이미지)</button>
     </div>
   );
 }
---
+++ src/api/axios.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+//DB 에 qr코드의 파라미터인 userEmail에 해당하는 카드 정보들을 가져오기 위해 이 api 를 사용합니다.
+
+// const host = window.location.hostname === "localhost" ? 'http://kimmyungsa.us-east-2.elasticbeanstalk.com' : "";
+
+//axios 인스턴스 생성
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000',//API baseURL
+    timeout:5000,
+    headers:{
+        'Content-Type': 'application/json', //응답헤더, json 형식
+    }
+
+})
+
+// 요청 인터셉터 추가
+api.interceptors.request.use(
+    (config) => {
+      console.log('Request Interceptor - Config:', config);
+      return config;
+    },
+    (error) => {
+      console.error('Request Interceptor - Error:', error);
+      return Promise.reject(error);
+    }
+  );
+  
+  // 응답 인터셉터 추가
+ api.interceptors.response.use(
+    (response) => {
+      console.log('Response Interceptor - Response:', response);
+      return response;
+    },
+    (error) => {
+        console.error('Response Interceptor - Error:', error);
+        console.error('Error status:', error.response.status);
+        console.error('Error data:', error.response.data);
+        return Promise.reject(error);
+    }
+  );
+
+
+export default api;
