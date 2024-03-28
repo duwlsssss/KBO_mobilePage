06cd8a9d21368a058c21a77525dec9cfe9179540
duwlsssss <rladuwls08114@gmail.com>
2024-03-28 12:26:28 +0900

    qr 1차 완성

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyCard from './components/MyCard/MyCard';
-import CardCollection from './components/CardCollection/CardCollection';
 import CardInfo from './components/CardInfo/CardInfo'
 
 function App() {
 
+  function setVHVariable() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  // 초기 설정
+  setVHVariable();
+  // 윈도우 리사이즈 이벤트가 발생할 때마다 setVHVariable를 호출하여 --vh 값을 업데이트
+  window.addEventListener('resize', setVHVariable);
+  
+
+  
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MyCard />} />
-        <Route path="/my-card" element={<MyCard />} />
-        <Route path="/card-collection" element={<CardCollection />} />
         <Route path="/card-info" element={<CardInfo />} /> 
       </Routes>
     </Router>
--- src/components/CardInfo/CardInfo.js
+++ src/components/CardInfo/CardInfo.js
@@ -3,40 +3,36 @@ import QRCode from 'qrcode.react';
 import api from '../../api/axios'
 import html2canvas from 'html2canvas';
 import saveAs from "file-saver";
-// import { , MobileView } from 'react-device-detect'
 import styles from './CardInfo.module.css';
-import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import  useUserEmailStore from '../../store/userEmail'
+import "98.css";
 
 function CardInfo() {
 
+  const {userEmail,setUserEmail} = useUserEmailStore();
   const [cards, setCards] = useState([]); //카드 저장용
+  const [cardImage, setCardImage] = useState(null);//이미지 저장
   const [isSaving, setIsSaving] = useState(false);//사진 저장 상태 추적
   const [isFlipped, setIsFlipped] = useState(false);
-  const location = useLocation();
-  const {userEmail,setUserEmail} = useUserEmailStore();
   const [showQR,setShowQR]=useState("false");
-
+  const location = useLocation();
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     const email = searchParams.get('userEmail');
     if (email) {
-      // console.log("email",email);
+      console.log("url에서 뽑은 userEmail",email);
       setUserEmail(email);
-      // console.log("userEmail",userEmail);
+      console.log("zustan에 저장된 userEmail",userEmail);
     }
-    //개빌용
-    // const email = "rladuwls0814@gmail.com";
-    // setUserEmail("rladuwls0814@gmail.com");
-    // console.log("email",email);
-  }, [location]);
+ }, []);
 
   //userEmail 변경시 호출됨
   useEffect(() => {
     if (userEmail) {
       fetchCards(userEmail);
+      fetchImages();
     }
   }, [userEmail]);
 
@@ -49,7 +45,7 @@ function CardInfo() {
           const userCards = data.data.filter(card => card.userEmail === userEmail);
           // 업데이트 날짜 기준으로 정렬
           userCards.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
-          // 가장 최근에 업데이트된 카드만 선택
+          // 같은 이메일 중 가장 최근에 업데이트된 카드 선택
           const latestCard = userCards[0] ? [userCards[0]] : [];
           setCards(latestCard);
         }
@@ -59,9 +55,22 @@ function CardInfo() {
       });
   };
 
-
-  //개발용 
-  // cards[0]={updatedAt:"2020.10.21",name:"rladuwls",school:"ssu", studentNum:"123", major:"aaa", email:"rladuwls0814@gmail.com", session:"bbb", MBTI:"infp", ig:"_chaeyes", moto:"ekeke" };
+  //서버에서 이미지 가져오기 
+  const fetchImages = async () => {
+    try {
+      const response = await api.get('/images', {
+        params: {
+          tags: userEmail
+        }
+        });
+      if (response.data && response.data.length > 0) {
+        // 이미지 데이터 배열 중 마지막 이미지의 URL을 사용
+        const lastImageIndex = response.data.length - 1;
+        setCardImage(response.data[lastImageIndex].url);
+      }
+    } catch (error) {
+      console.error('Error fetching images:', error);
+  }};
 
   //이미지로 저장
   const frontRef = useRef(null); 
@@ -72,15 +81,14 @@ function CardInfo() {
     setIsSaving(true); // 사진 저장 시작
     console.log("사진 저장 실행");
 
-    // 카드를 앞면으로 설정
-    setIsFlipped(false);
-    // 앞면이 화면에 표시되기를 잠시 기다림
-    await new Promise(resolve => setTimeout(resolve, 800));
-    if (frontRef.current) {
-      try {
-        const canvasFront = await html2canvas(frontRef.current, { scale: 2 });
+    const waitForRender = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    };
 
-        const dataUrl = canvasFront.toDataURL();
+    const captureCardImage = async (element, filename) => {
+      try {
+        const canvas = await html2canvas(element, { scale: 2, useCORS: true });
+        const dataUrl = canvas.toDataURL();
         const rotatedImage = new Image();
         rotatedImage.onload = function() {
           const rotatedCanvas = document.createElement('canvas');
@@ -94,57 +102,34 @@ function CardInfo() {
 
           rotatedCanvas.toBlob(function(blob) {
             if (blob) {
-              saveAs(blob, "card-front.png");
+              saveAs(blob, filename);
             }
           });
         };
         rotatedImage.src = dataUrl;
       } catch (error) {
-          console.error("Problem saving front of card:", error);
+        console.error("Error saving card image:", error);
       }
-    } 
-    // 카드를 뒷면으로 설정
-    setIsFlipped(true);
-    // 뒷면이 화면에 표시되기를 잠시 기다림
-    await new Promise(resolve => setTimeout(resolve, 800)); 
+    };
 
+    setIsFlipped(false);
+    await waitForRender();
+
+    if (frontRef.current) {
+      await captureCardImage(frontRef.current, "card-front.png");
+    } 
+
+    setIsFlipped(true);
+    await waitForRender();
 
     if (backRef.current) {
-      try {
-        // 뒷면이 보이도록 상태 변경
-        setIsFlipped(true); // 필요한 경우 상태를 이용하여 뒷면을 보이게 할 수 있습니다.
+      await captureCardImage(backRef.current, "card-back.png");
+    } 
 
-        // 애니메이션 완료 후 잠시 기다린 다음 캡처
-        await new Promise(resolve => setTimeout(resolve, 800)); // 다시 한 번 애니메이션 시간만큼 기다립니다.
-
-          const canvasBack = await html2canvas(backRef.current, { scale: 2 });
-          const dataUrl = canvasBack.toDataURL();
-          const rotatedImage = new Image();
-          rotatedImage.onload = function() {
-            const rotatedCanvas = document.createElement('canvas');
-            rotatedCanvas.width = rotatedImage.height;
-            rotatedCanvas.height = rotatedImage.width;
-
-            const context = rotatedCanvas.getContext('2d');
-            context.translate(rotatedCanvas.width / 2, rotatedCanvas.height / 2);
-            context.rotate(-90 * Math.PI / 180);
-            context.drawImage(rotatedImage, -rotatedImage.width / 2, -rotatedImage.height / 2);
-
-            rotatedCanvas.toBlob(function(blob) {
-              if (blob) {
-                saveAs(blob, "card-back.png");
-              }
-            });
-          };
-          rotatedImage.src = dataUrl;
-      } catch (error) {
-          console.error("Problem saving back of card:", error);
-      }
-  }
-  // 마무리로 카드를 원래 상태로 돌려놓기
-  setIsFlipped(false); 
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  setIsSaving(false); // 사진 저장 종료
+    // Clean up and set states back to initial values
+    setIsFlipped(false);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setIsSaving(false); 
 };
 
 
@@ -163,8 +148,6 @@ const handleIgClick = () => {
   }
 };
 
-
-
   //카드 뒤집기 애니메이션 
   const handleCardClick = () => {
     //앞면이면
@@ -174,7 +157,7 @@ const handleIgClick = () => {
       // 애니메이션 지속 시간이 0.5초일 때
       setTimeout(() => {
         setShowQR(false);
-      }, 200); 
+      }, 150); 
     }
     
     // 카드의 뒤집힌 상태를 토글
@@ -188,49 +171,68 @@ const handleIgClick = () => {
       // 애니메이션 지속 시간이 1초일 때
       setTimeout(() => {
         setShowQR(true);
-      }, 200); 
+      }, 150); 
     }
   };
 
 
 
+
   return (
-    <div>
-        {isSaving && <div className={styles.savingPopup}>사진 저장 중...</div>}
-        {userEmail}에 해당하는 카드 출력 
-        {cards.length > 0 ? (
-          <div className={styles.card}>
-              <div className={`${styles.cardFront} ${isFlipped ? styles.flipped : ''}`} ref={frontRef}>
-                <div className={styles.infoContainer}>
-                  <div className={`${styles.infoItem} ${styles.date}`}>
-                    {cards[0].updatedAt ? new Date(cards[0].updatedAt).toLocaleDateString() : 'N/A'}
-                  </div>
-                  <div className={`${styles.infoItem} ${styles.name}`}>{cards[0].name || 'N/A'}</div>
-                  <div className={`${styles.infoItem} ${styles.school}`}>{cards[0].school || 'N/A'}</div>
-                  <div className={`${styles.infoItem} ${styles.studentNum}`}>{cards[0].studentNum || 'N/A'}</div>
-                  <div className={`${styles.infoItem} ${styles.major}`}>{cards[0].major || 'N/A'}</div>
-                  <div className={`${styles.infoItem} ${styles.email}`} onClick={handleEmailClick}>{cards[0].email || 'N/A'}</div>
-                  <div className={`${styles.infoItem} ${styles.session}`}>{cards[0].session || 'N/A'}</div>
-                  <div className={`${styles.infoItem} ${styles.MBTI}`}>{cards[0].MBTI || 'N/A'}</div>
-                  <div className={`${styles.infoItem} ${styles.IG}`} onClick={handleIgClick}>{cards[0].ig || 'N/A'}</div>
-                  <div className={`${styles.infoItem} ${styles.moto}`}>{cards[0].moto || 'N/A'}</div>
-                </div>
-              </div>
-              <div className={`${styles.cardBack} ${isFlipped ? styles.flipped : ''}`} ref={backRef}>
-              {showQR && (
-                <div className={styles.QR}>
-                  <QRCode value={`http://localhost:3001/card-info?userEmail=${userEmail}`} />
-                  {/* <QRCode value={`https://kimmobile.netlify.app/card-info?userEmail=${userEmail}`} /> */}
-                </div>
-              )}
+     <>
+          <div style={{ width: 350, marginTop:5, marginBottom:5}} className='window'>
+              <div className="title-bar">
+              <span className={styles.addressText}>https://www.kimsoffice.com :: 김씨네 <span className={styles.addressTextColor}> 명함제작 </span> 사무소</span>
+              <div style={{paddingTop:5, paddingBottom:5}}className="title-bar-controls">
+                <button aria-label="Minimize" />
+                <button aria-label="Maximize" />
+                <button aria-label="Close" />
               </div>
             </div>
-        ) : (
-          <div>{userEmail}에 해당하는 카드 없음</div>
-        )}
-        <button type="button" onClick={handleCardClick}>카드뒤집기</button>
-        <button type="button" onClick={saveCardAsImage}>저장하기(이미지)</button>
-      </div>
+            <div className={styles.contentArea}>
+                {isSaving && <div className={styles.savingPopup}>사진 저장 중...</div>}
+                {/* {userEmail}에 해당하는 카드 출력 */}
+                {cards.length > 0 ? (
+                  <>
+                    <div className={styles.ownerText}><span className={styles.ownerTextStrong}>{cards[0].name}</span> 님의 명함</div>
+                    <div className={styles.card}>
+                        <div className={`${styles.cardFront} ${isFlipped ? styles.flipped : ''}`} ref={frontRef}>
+                          <div className={styles.infoContainer}>
+                            <div className={`${styles.infoItem} ${styles.date}`}>
+                              {cards[0].updatedAt ? new Date(cards[0].updatedAt).toLocaleDateString() : 'N/A'}
+                            </div>
+                            <div className={`${styles.infoItem} ${styles.name}`}>{cards[0].name || 'N/A'}</div>
+                            <div className={`${styles.infoItem} ${styles.engName}`}>{cards[0].engName || 'N/A'}</div>
+                            <div className={`${styles.infoItem} ${styles.school}`}>{cards[0].school || 'N/A'}</div>
+                            <div className={`${styles.infoItem} ${styles.studentNum}`}>{cards[0].studentNum || 'N/A'}</div>
+                            <div className={`${styles.infoItem} ${styles.major}`}>{cards[0].major || 'N/A'}</div>
+                            <div className={`${styles.infoItem} ${styles.email}`} onClick={handleEmailClick}>{cards[0].email || 'N/A'}</div>
+                            <div className={`${styles.infoItem} ${styles.session}`}>{cards[0].session || 'N/A'}</div>
+                            <div className={`${styles.infoItem} ${styles.MBTI}`}>{cards[0].MBTI || 'N/A'}</div>
+                            <div className={`${styles.infoItem} ${styles.IG}`} onClick={handleIgClick}>@{cards[0].ig || 'N/A'}</div>
+                            <div className={`${styles.infoItem} ${styles.moto}`}>{cards[0].moto || 'N/A'}</div>
+                            {cardImage && <img src={cardImage} alt="Profile" className={styles.cardImage} />}
+                          </div>
+                        </div>
+                        <div className={`${styles.cardBack} ${isFlipped ? styles.flipped : ''}`} ref={backRef}>
+                        {showQR && (
+                          <div className={styles.QR}>
+                            <QRCode value={`https://kimmobile.netlify.app/card-info?userEmail=${userEmail}`} />
+                          </div>
+                        )}
+                        </div>
+                      </div>
+                  </>
+                ) : (
+                  <div>{userEmail}에 해당하는 카드 없음</div>
+                )}
+                <div className="field-row" style={{ justifyContent: "center"}}>
+                  <button type="button" onClick={handleCardClick}>카드뒤집기</button>
+                  <button type="button" onClick={saveCardAsImage}>저장하기(이미지)</button>
+                </div>
+              </div>
+          </div>
+     </>
   );
 }
 
